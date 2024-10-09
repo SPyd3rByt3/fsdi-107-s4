71cc5283785a5d43eb8db0d283fcbafb8f935c57
SPyd3rByt3 <DailyDoseOfSDGKU@gmail.com>
2024-10-08 19:40:25 -0700

    object.literals

---
+++ OneDrive/petSalon/scripts/practice.js
@@ -0,0 +1,10 @@
+//object Literala
+let student1 = {
+    name:"jon Smith",
+    age:99,
+    isAdmin:false,
+    grade1:3.7
+}
+
+console.log(student1);
+
---
+++ OneDrive/petSalon/scripts/register.js
@@ -0,0 +1,27 @@
+console.log("register.js");
+let pets = []; //empty array
+//create pets
+let pet1 = {
+    name:"Q",
+    age:99,
+    gender:"male"
+}
+
+let pet2 = {
+    name:"Squirrely",
+    age:11,
+    gender:"male"
+}
+
+function displayName(){
+    //display pet names
+    console.log(pets[0].name);
+    console.log(pets[1].name);
+    
+    
+}
+
+//HINTS:
+// use a for loop and travel the array
+// use brute force
+// length the amount of elements on the array
